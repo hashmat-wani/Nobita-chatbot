29dd461e7d88e5281af86cd4024827e6adebc493
Hashmat-Noorani <hashmatwani@icloud.com>
2023-01-17 00:02:23 +0530

    testing

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -207,6 +207,21 @@ const SideBar = ({
               <option value={10}>Ten</option>
               <option value={20}>Twenty</option>
               <option value={30}>Thirty</option>
+              <option value={10}>Ten</option>
+              <option value={20}>Twenty</option>
+              <option value={30}>Thirty</option>
+              <option value={10}>Ten</option>
+              <option value={20}>Twenty</option>
+              <option value={30}>Thirty</option>
+              <option value={10}>Ten</option>
+              <option value={20}>Twenty</option>
+              <option value={30}>Thirty</option>
+              <option value={10}>Ten</option>
+              <option value={20}>Twenty</option>
+              <option value={30}>Thirty</option>
+              <option value={10}>Ten</option>
+              <option value={20}>Twenty</option>
+              <option value={30}>Thirty</option>
               {/* {models.map((model) => (
                 <option key={model.id} value={model.id}>
                   {model.id}
